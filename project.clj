816de05dd94f24d5552990bb59a9162b28d1962e
Sweta Shrestha <sweta.shrestha.yipl.com.np>
2015-04-16 23:07:02 +0545

    working on todo mvc

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [figwheel "0.2.3-SNAPSHOT"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [sablono "0.2.3"]
-                 [org.omcljs/om "0.8.6"]]
+                 [org.omcljs/om "0.8.6"]
+                 [alandipert/storage-atom "1.2.4"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]
             [lein-figwheel "0.2.3-SNAPSHOT"]]
@@ -17,7 +18,7 @@
   :source-paths ["src"]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled"]
-  
+
   :cljsbuild {
     :builds [{:id "dev"
               :source-paths ["src" "dev_src"]
@@ -32,13 +33,13 @@
              {:id "min"
               :source-paths ["src"]
               :compiler {:output-to "resources/public/js/compiled/om_tut.js"
-                         :main om-tut.core                         
+                         :main om-tut.core
                          :optimizations :advanced
                          :pretty-print false}}]}
 
   :figwheel {
-             :http-server-root "public" ;; default and assumes "resources" 
-             :server-port 3449 ;; default
+             :http-server-root "public" ;; default and assumes "resources"
+             :server-port 3447 ;; default
              :css-dirs ["resources/public/css"] ;; watch and update CSS
 
              ;; Server Ring Handler (optional)
@@ -60,5 +61,5 @@
              ;; :repl false
 
              ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log" 
+             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
              })
--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -1,6 +1,8 @@
 (ns ^:figwheel-always om-tut.core
-    (:require[om.core :as om :include-macros true]
-             [sablono.core :refer-macros [html]]))
+  (:require[om.core :as om :include-macros true]
+           [sablono.core :refer-macros [html]]
+           [alandipert.storage-atom :refer [local-storage]]
+           ))
 
 (enable-console-print!)
 
@@ -8,15 +10,134 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello World!"}))
+;;(defonce app-state (atom {:list ["Red" "Yellow" "Green" "Blue" "Orange"]}))
 
-(defn hello-component
+(defonce app-state
+  (local-storage
+   (atom
+    {:todos
+     [{:text "A must to do" :done true}
+      {:text "Either do or not" :done false}
+      {:text "Dont do it" :done false}
+      ]})
+   :todos-app-state))
+
+
+(defn todo-item
+  [todo owner]
+  (om/component
+   (let [cls (if (:done todo) "done" "")
+         toggle (fn [todo] (update todo :done not))]
+     (html
+      [:li {:class "todo"}
+       [:input {:type "checkbox" :class "toggle" :checked (:done todo)
+                :on-click #(om/transact! todo toggle)}]
+       [:span {:class cls} (:text todo)]
+       ]
+      )
+     )
+   )
+  )
+
+(defn add-todo [text todos]
+  (conj todos {:text text :done false})
+  )
+
+(defn todo-adder [todos owner]
+  (om/component
+   (html
+    [:input
+     {:type "text"
+      :class "do-btn"
+      :placeholder "What needs doing?"
+      :on-key-up (fn [event]
+                   (let [input (.-target event)]
+                     (when (= 13 (.-keyCode event)) ;; ENTER
+                       (om/transact! todos
+                                     (partial add-todo (.-value input)))
+                       (set! (.-value input) ""))))}])))
+
+(defn count-todo [todos owner]
+  (om/component
+   (html
+    [:div {:class "items-wrap"}
+     [:div {:class "items-done"} "items done:" (count (filter :done todos))]
+     [:div {:class "items-left"} "items left:" (count (remove :done todos))]
+     ]
+    )
+   )
+  )
+
+(defn clear-todo-button [todos]
+  (vec(remove :done todos))
+  )
+
+(defn clear-todo [todos owner]
+  (om/component
+   (html
+    [:input {:type "button"
+             :class (if (zero? (count (filter :done todos))) "hidden" "clear-btn")
+             :value "Clear Completed"
+             :on-click #(om/transact! todos clear-todo-button)
+             }]
+    )
+   )
+  )
+
+(defn add-filters [todos owner]
+  (om/component
+   (html
+    [:div {:class "filter-wrapper"}
+     [:ul
+      [:li
+       [:a {:href "#"} "All"]]
+      [:li
+       [:a {:href "#"} "Active"]]
+      [:li
+       [:a {:href "#"} "Completed"]
+       ]
+      ]
+     ]
+    )
+   )
+  )
+
+(defn todo-list
   [data owner]
   (om/component
-   (html [:h1 (:text data)])))
+   (html
+    [:div {:class "wrapper"}
+     [:h1 "Things to be done"]
+     [:ul
+      (om/build-all todo-item (:todos data))
+      ]
+     (om/build todo-adder (:todos data))
+     (om/build count-todo (:todos data))
+     (om/build clear-todo (:todos data))
+     (om/build add-filters (:todos data))
+     ]
+    )
+   ))
 
-(om/root hello-component
+;;(defn colors-view
+;;  [data owner]
+;; (om/component
+;; (html
+;; [:div {:class "wrapper"}
+;; [:h1 "List of Colors"]
+;;[:ul {:class "colors-list"}
+;;(map (fn[colors] [:li [:a {:href "#"} colors]]) (:list data))
+;;]])))
+
+;; (om/root colors-view
+;;       app-state
+;;         {:target (. js/document (getElementById "app0"))})
+
+
+(om/root todo-list
          app-state
-         {:target (. js/document (getElementById "app"))})
+         {:target (. js/document (getElementById "todos"))})
+
+
 
 
